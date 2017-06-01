cf80a2f36b1225991b39dfbcaeae8643c48043dd
Yann Coulon <coulon.yann@gmail.com>
2017-06-01 20:24:08 +0200

    Import project from spring initializr

---
+++ src/main/java/io/github/ycoulon/springbootcxftest/SpringBootCxfTestApplication.java
@@ -0,0 +1,12 @@
+package io.github.ycoulon.springbootcxftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCxfTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCxfTestApplication.class, args);
+	}
+}
